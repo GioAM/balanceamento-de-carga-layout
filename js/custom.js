e193b41c46bfbe9d9fc141bd68e3688e7b393a65
giovani.meneguel <gio_meneguel@hotmail.com>
2019-05-29 10:59:22 -0300

    Alterações na tabela

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,7 @@ var mpsoc_y = 0;
 var tasks_per_pe = 0;
 
 function start(){
+   $("#tables").empty();
   getProcesses();
 }
 function Process(processObject, title) {
@@ -18,6 +19,11 @@ function Processor(activity, processors, load, enabled) {
   this.load = load;
   this.enabled = enabled;
 }
+function ItemArray(x, y, load) {
+	this.x = x;
+  this.y = y;
+  this.load = load;
+}
 function getProcesses(){
    for(var a = 0; a < tests.length; a++){
      id = tests[a].id;
@@ -28,6 +34,7 @@ function getProcesses(){
      tasks_per_pe = tests[a].tasks_per_pe;
      var processesArray = [];
      var processors = [];
+     var arrayProcessors = [];
      for(var b = 0; b < tests[a].apps.length; b ++){
        var qtd_apps = tests[a].apps[b].qtd_apps;
        var app_name = tests[a].apps[b].app_name;
@@ -42,35 +49,85 @@ function getProcesses(){
          }
        }
      }
+    var gmpsOk = false;
+    var gmpsNumber = 0;
     for(var f = 0; f < mpsoc_x; f++){
       processors[f] = [];
       for(var g = 0; g < mpsoc_y; g++){
-        if(processesArray.length > 0){
-          itemProcessor = new Processor(processesArray[0].title, 1, processesArray[0].processObject.load,true);
-          processors[f].push(itemProcessor);
-          processesArray.shift();
-          if(processors[f][g].processors >= tasks_per_pe){
-            processors[f][g].enabled = false;
+        if(gmpsOk){
+          if(processesArray.length > 0){
+            itemProcessor = new Processor(processesArray[0].title, 1, processesArray[0].processObject.load,true);
+            processors[f].push(itemProcessor);
+            processesArray.shift();
+            if(processors[f][g].processors >= tasks_per_pe){
+              processors[f][g].enabled = false;
+            }
+            itemArray = new ItemArray(f,g,processors[f][g].load);
+            arrayProcessors.push(itemArray);
+          }else{
+            itemProcessor = new Processor("-", 0, 0,true);
+            processors[f].push(itemProcessor);
           }
         }else{
-          itemProcessor = new Processor("-", 0, 0,true);
+          gmpsNumber++;
+          itemProcessor = new Processor("GMP", 0, "-",false);
           processors[f].push(itemProcessor);
+          if(gmpsNumber >= ((mpsoc_y * mpsoc_x)/(cluster_x * cluster_y))){
+            gmpsOk=true;
+          }
         }
       }
     }
-    console.log("montando");
+    arrayProcessors.sort(compareLoad);
+    while(processesArray.length > 0){
+      var index = 0;
+      var alocatted = false;
+      arrayProcessors.sort(compareLoad);
+      while(!alocatted){
+        var x = arrayProcessors[index].x;
+        var y = arrayProcessors[index].y;
+        var load = processesArray[0].processObject.load;
+        if(processors[x][y].enabled){
+          processors[x][y].processors = processors[x][y].processors + 1;
+          processors[x][y].load = processors[x][y].load + load;
+          processors[x][y].activity = processors[x][y].activity + " / " + processesArray[0].title;
+          arrayProcessors[index].load = processors[x][y].load;
+          if(processors[x][y].processors >= tasks_per_pe){
+            processors[x][y].enabled = false;
+          }
+          alocatted = true;
+          processesArray.shift();
+        }else{
+          index++;
+        }
+      }
+    }
     drawTable(processors);
    }
  }
  function drawTable(matriz){
-   $('#tables').append("<h1> Teste " + id + "</h1>");
+   $('#tables').append("<h1> Teste #" + id + "</h1>");
    $('#tables').append("<table class='table table-hover'><tbody id='table" + id + "'></tbody></table>");
    for(var h = 0; h < mpsoc_x; h++){
      $('#table'+id).append("<tr>");
      for(var i = 0; i < mpsoc_y; i++){
+       var hasContent = matriz[h][i].enabled;
+       if(matriz[h][i].load <= 0){
+         hasContent = "noContent";
+       }
+       if(matriz[h][i].activity == "GMP"){
+         hasContent = "GMP";
+       }
        if(matriz[h][i] != null){
-         $('#table'+id).append("<td id='row"+ h + "_"+ i +"' class='"+ matriz[h][i].enabled +"' >"+ matriz[h][i].activity +"</br> Load: " + matriz[h][i].load +"</td>");
+         $('#table'+id).append("<td id='row"+ h + "_"+ i +"' class='"+ hasContent +"' >"+ matriz[h][i].activity +"</br> Load: " + matriz[h][i].load +"</td>");
        }
      }
    }
  }
+function compareLoad(processorA,processorB){
+  if (processorA.load < processorB.load)
+     return -1;
+  if (processorA.load > processorB.load)
+    return 1;
+  return 0;
+}
